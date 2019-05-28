2aca1ae7ca9a7f8e082509447b3d2e1f1d614df7
Guilherme Fernando Angelico <angelicogfa@gmail.com>
2019-05-28 17:00:21 -0300

    Inclusão de tags para jupiter notbook

--- modules/deep_learning/deep_learning.py
+++ modules/deep_learning/deep_learning.py
@@ -1,4 +1,4 @@
-# Import
+#%% Import
 import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
@@ -7,28 +7,28 @@ import numpy as np
 from sklearn.metrics import confusion_matrix
 from keras.datasets import mnist
 
-# Carregamento dos dados
+#%% Carregamento dos dados
 (X_treinamento, y_treinamento), (X_teste, y_teste) = mnist.load_data()
 plt.imshow(X_treinamento[150], cmap='gray')
 plt.title(y_treinamento[150])
 
-# Reshape dos dados
+#%% Reshape dos dados
 X_treinamento = X_treinamento.reshape((len(X_treinamento), np.prod(X_treinamento.shape[1:])))
 X_teste = X_teste.reshape((len(X_teste), np.prod(X_teste.shape[1:])))
 
-# Conversão do tipo dos dados
+#%% Conversão do tipo dos dados
 X_treinamento = X_treinamento.astype('float32')
 X_teste = X_teste.astype('float32')
 
-# Redução do tamanho dos dados, visto que o tamanho máximo do conteudo para imagem é 255
+#%% Redução do tamanho dos dados, visto que o tamanho máximo do conteudo para imagem é 255
 X_treinamento /= 255
 X_teste /= 255
 
-# Criação da matrix de resultado para o neuronio de saida
+#%% Criação da matrix de resultado para o neuronio de saida
 y_treinamento = np_utils.to_categorical(y_treinamento, 10)
 y_teste = np_utils.to_categorical(y_teste, 10)
 
-# Criação do modelo
+#%% Criação do modelo
 modelo = Sequential()
 # Adiciona a primeira camada com 64 neuronios recebendo 784 inputs
 # Função de ativação: Relu - Passa o valor se ele for positivo e se for negativo
@@ -45,13 +45,13 @@ modelo.add(Dropout(0.2))
 
 modelo.add(Dense(units=10, activation='softmax'))
 
-# Obtem o sumario do modelo
+#%% Obtem o sumario do modelo
 modelo.summary()
 
 modelo.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 historico = modelo.fit(X_treinamento, y_treinamento, epochs=50, validation_data=(X_teste, y_teste))
 
-# Obtem os dados historicos utilizados
+#%% Obtem os dados historicos utilizados
 historico.history.keys()
 plt.plot(historico.history['val_loss'])
 plt.plot(historico.history['val_acc'])
