a70b40433d7dd42882b505c02ade695cc588209a
Guilherme Fernando Angelico <angelicogfa@gmail.com>
2019-06-06 15:57:47 -0300

    inclusão de mini projeto

--- modules/mini-projetos/kaggle-autos.py
+++ modules/mini-projetos/kaggle-autos.py
@@ -73,6 +73,49 @@ g.ax.set_title('Veiculos por marca')
 plt.show()
 
 #%% Preço médio dos veículos com base no tipo de veículo, bem como no tipo de caixa de câmbio
-# Crie um Plot com o Preço médio dos veículos com base no tipo de veículo, bem como no tipo de caixa de câmbio
+# Crie um Plot com o Preço médio dos veículos com base no tipo de veículo, bem como no tipo 
+# de caixa de câmbio
 
+df.head()
+plt.figure(figsize=(12,8))
+axies = sns.barplot(x='vehicleType', y='price', data=df, hue='gearbox')
+axies.set_title('Preço Médio dos veículos por tipo de veículo e tipo de caixa de câmbio')
+axies.xaxis.set_label_text('Tipo de veículo', fontdict = { 'size':12})
+axies.yaxis.set_label_text('Preço médio', fontdict = { 'size':12})
+plt.show()
 
+#%% Potência média de um veículo por tipo de veículo e tipo de caixa de câmbio
+# Crie um Barplot com a Potência média de um veículo por tipo de veículo e tipo de caixa de câmbio
+
+df.head()
+plt.figure()
+axies = sns.barplot(x= 'vehicleType', y  = 'powerPS', data=df, hue='gearbox')
+axies.set_title('Potência média do veículo por tipo de veiculo e tipo de caixa de câmbio')
+axies.xaxis.set_label_text('Tipo de veículos', fontdict = { 'size':12})
+axies.yaxis.set_label_text('Potência Média', fontdict = { 'size':12})
+plt.show()
+
+#%% Calulos
+# Calcule a média de preço por marca e por veículo
+media = pd.DataFrame()
+
+for b in list(df['brand'].unique()):
+    for vt in list(df['vehicleType'].unique()):
+        preco = df[(df['brand'] == b) & (df['vehicleType'] == vt)]['price'].mean()
+        media = media.append(pd.DataFrame({ 'brand': b, 'vehicleType': vt, 'avgPrice': preco }, index=[0]))
+media = media.reset_index()
+del media['index']
+media['avgPrice'].fillna(0, inplace=True)
+media['avgPrice'].isnull().value_counts()
+media['avgPrice'] = media['avgPrice'].astype(int)
+media.head()
+
+#%% Preço médio de um veículo por marca, bem como tipo de veículo
+# Crie um Heatmap com Preço médio de um veículo por marca, bem como tipo de veículo
+med = media.pivot('brand', 'vehicleType', 'avgPrice')
+figure, ax = plt.subplots(figsize=(15,20))
+sns.heatmap(med, linewidths=1, cmap='YlGnBu', annot=True, ax=ax, fmt='d')
+ax.set_title('Heatmap - Preço médio de um veículo por marca e tipo de veículo', fontdict={ 'size': 20 })
+ax.xaxis.set_label_text('Tipo de Veículo', fontdict={ 'size': 20 })
+ax.yaxis.set_label_text('Marca', fontdict={ 'size': 20 })
+plt.show()
